3644c5b6757d56363f2784c19c31f0e7aa5470ab
VAA <VasilyevAA>
2020-04-07 21:59:20 +0300

    add base urls for api and ui add client_id

--- setting.py
+++ setting.py
@@ -3,3 +3,7 @@ import os
 BROWSERSTACK_USERNAME = os.getenv("BROWSERSTACK_USERNAME", "")
 BROWSERSTACK_ACCESS_KEY = os.getenv("BROWSERSTACK_ACCESS_KEY", "")
 
+BASE_UI_TEST_URL = os.getenv("UI_TEST_URL", "https://dev.whisk.com")
+BASE_API_TEST_URL = os.getenv("UI_TEST_URL", "https://graph.whisk.com")
+
+WHISK_CLIENT_ID = os.getenv("WHISK_CLIENT_ID", "")
