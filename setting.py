2aab6d7dbef9aef7bbbdeecf9c0ecadeed1a1a55
VAA <VasilyevAA>
2020-04-08 14:01:49 +0300

    add delete item test

--- setting.py
+++ setting.py
@@ -6,4 +6,4 @@ BROWSERSTACK_ACCESS_KEY = os.getenv("BROWSERSTACK_ACCESS_KEY", "")
 BASE_UI_TEST_URL = os.getenv("UI_TEST_URL", "https://dev.whisk.com")
 BASE_API_TEST_URL = os.getenv("UI_TEST_URL", "https://graph-dev.whisk.com")
 
-WHISK_CLIENT_ID = os.getenv("WHISK_CLIENT_ID", "")
+WHISK_CLIENT_ID = os.getenv("WHISK_CLIENT_ID", "WCqJWnpNatcf3LUCxZmq94pR30sj2OOdBbMoGO8NGrMgUMk6Ogl4EMvLqcykNuGf")
---
+++ tests/api/test_delete_item_from_shoping_list.py
@@ -0,0 +1,6 @@
+
+
+class TestDeleteItemFromShoppingList:
+
+    def test_positive_delete_item_from_shopping_list(self):
+        raise NotImplementedError("API doesn't support delete item from shopping list")
