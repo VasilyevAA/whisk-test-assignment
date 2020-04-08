bdec64a71e4fdf9c6ca992c29ba7c512bf7eb8e4
VAA <VasilyevAA>
2020-04-08 12:32:53 +0300

    add assertion checkers for list small refactor clientApi add bool and number random generators

---
+++ utils/checks.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import pytest
+from datatest import validate, accepted, Extra
+
+
+def eq_list(actual: List, expected: List):
+    """
+    lists should be sorted
+
+    This solution have many problem, but for good solution need use complex methods and libs
+    for create common "validator" for different data structures
+    """
+    assert len(actual) == len(expected), "Lists have different length"
+    for i, ex_data in enumerate(expected):
+        with accepted(Extra):
+            validate(actual[i], ex_data, "")
+
+
+class TestCheckAsserts:
+
+    def test_positive_eq_list_required_have_missing(self):
+        data = [{'id': 11, 'qwe': 12312}]
+
+        requirement = [{'id': 11}]
+        eq_list(data, requirement)
+
+    def test_negative_eq_list_required_have_diff_value_in_required(self):
+        data = [{'id': 11, 'qwe': 12312}]
+        requirement = [{'id': 11, 'qwe': 333333}]
+        with pytest.raises(AssertionError):
+            eq_list(data, requirement)
+
+    def test_negative_eq_list_diff_length(self):
+        data = [{'id': 11, 'qwe': 12312}]
+        requirement = [{'id': 11}, {'id': 22}]
+        with pytest.raises(AssertionError):
+            eq_list(data, requirement)
+
+
+if __name__ == '__main__':
+    from utils import run_test
+    run_test(__file__)
--- utils/generate.py
+++ utils/generate.py
@@ -12,3 +12,14 @@ def generate_value(size=10, chars=string.ascii_letters + string.digits, prefix='
     size = size - len_pref - len_suff
     random_string = ''.join([prefix] + [random.choice(chars) for _ in range(size)] + [suffix])
     return random_string
+
+
+def get_rnd_bool():
+    return bool(random.getrandbits(1))
+
+
+def generate_number(min=0, max=10, is_float=False, decimals=2):
+    if is_float:
+        return round(random.uniform(min, max), decimals)
+    else:
+        return random.randint(min, max)
