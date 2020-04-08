494f353e898baba508552be16f2990be810117de
VAA <VasilyevAA>
2020-04-09 01:00:43 +0300

    check popup in signup test

--- tests/ui/test_singup_user.py
+++ tests/ui/test_singup_user.py
@@ -1,3 +1,4 @@
+from actions_ui.common import PopupMsgTempaltes
 from actions_ui.shopping_list import sign_up_user
 from actions_ui.page_objects.shipping_list_page import ShoppingListPage
 
@@ -5,6 +6,7 @@ from actions_ui.page_objects.shipping_list_page import ShoppingListPage
 def test_signup_user(browser):
     shopping_list = ShoppingListPage(browser, True)
     username = sign_up_user(shopping_list)
+    shopping_list.wait_popup_hidden(PopupMsgTempaltes.SIGN_UP)
     active_username = shopping_list.get_active_username()
     assert username == active_username
 
